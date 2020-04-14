a21fb38af9684213ff6dc9c5c81fa92e96587539
JT <foosyou@gmail.com>
2020-04-14 09:59:48 -0500

    kickoff function

--- info_grabber.py
+++ info_grabber.py
@@ -6,12 +6,65 @@ Cisco network devices and save the output to file. Devices and parameters are
 provided by the SimpleInventory plugin for Nornir using YAML files. 
 '''
 
+import sys
+from getpass import getpass
 from datetime import datetime
 from nornir import InitNornir
 from nornir.core.filter import F
 from nornir.plugins.tasks import text, files
 from nornir.plugins.functions.text import print_result
 from nornir.plugins.tasks.networking import netmiko_send_command
+
+
+# print formatting function
+def c_print(printme):
+    # Print centered text with newline before and after
+    print(f"\n" + printme.center(80, ' ') + "\n")
+
+
+# Nornir kickoff
+def kickoff():
+    # print banner
+    print()
+    print('~'*80)
+    c_print('This script will gather discovery information from Cisco devices')
+
+    if len(sys.argv) < 2:
+        site = ""
+
+    else:
+        site = sys.argv[1] + "_"
+
+    # initialize The Norn
+    nr = InitNornir(
+        inventory={
+            "plugin": "nornir.plugins.inventory.simple.SimpleInventory",
+            "options": {
+                "host_file": f"inventory/{site}hosts.yaml",
+                "group_file": f"inventory/{site}groups.yaml",
+                "defaults_file": "inventory/defaults.yaml"
+            }
+        }
+    )
+    
+    # filter The Norn
+    nr = nr.filter(platform="cisco_ios")
+
+    c_print('Checking inventory for credentials')
+    # check for existing credentials in inventory
+
+    if nr.inventory.defaults.username == None or nr.inventory.defaults.password == None:
+        c_print('Please enter device credentials:')
+
+    if nr.inventory.defaults.username == None:
+        nr.inventory.defaults.username = input("Username: ")
+    
+    if nr.inventory.defaults.password == None:
+        nr.inventory.defaults.password = getpass()
+        print()
+    print('~'*80)
+    return nr
+
 
 def grab_info(task):
     # show commands to be run
@@ -33,7 +86,7 @@ def grab_info(task):
         "show cdp neighbors detail",
         ]
 
-    print(f"Collecting data from {task.host}")
+    c_print(f"*** Collecting data from {task.host} ***")
 
     # loop over commands
     for cmd in commands:
@@ -50,11 +103,10 @@ def grab_info(task):
             append=True
         )
 
+
 def main():
-    # initialize The Norn
-    nr = InitNornir()
-    # filter The Norn
-    nr = nr.filter(platform="cisco_ios")
+    # kickoff The Norn 
+    nr = kickoff()
     # run The Norn
     nr.run(task=grab_info)
 
